d2456da142b17fab69b53cb40cc8f4a12c96b663
damiicodes <93157277+damiicodes@users.noreply.github.com>
2024-11-04 11:48:51 +0000

    Update setup.sh
    adding some more bash commands

--- setup.sh
+++ setup.sh
@@ -5,3 +5,21 @@ ls
 ls
 
 touch file.txt
+
+if [ -f "file.txt" ]; then
+  echo "File exists"
+else
+  echo "File does not exist"
+fi
+
+name="Dami"
+echo "Hello, $name" > file.txt
+
+cat file.txt
+
+
+for i in {1..5}; do
+  echo "Iteration $i"
+done
+
+
